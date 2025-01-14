c7b57c38c44b728da02b20d3ca011bc398a9e937
Suzy Kenne <suzyndatewo@gmail.com>
2025-01-14 11:38:44 +0100

    added logout controller

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,50 +2,52 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Models;
+use App\Models\User;
 use Illuminate\Http\Request;
-use Iluminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Validator;
 
 class ApiController extends Controller
 {
-    public function login(Request $request){
+    public function login(Request $request)
+    {
         $validator = Validator::make($request->all(), [
-            'email' => 'required:email',
+            'email' => 'required|email',
             'password' => 'required|string|min:6'
         ]);
 
-        if($validator->fails()){
+        if ($validator->fails()) {
             return response()->json([
                 'message' => $validator->errors()->first()
             ], 401);
         }
 
         $credentials = request(['email', 'password']);
-        if(!auth()->attempt($credentials)){
+        
+        if (!auth()->attempt($credentials)) {
             return response()->json([
-                'message' => "Unauthorized, check your login credentials",
+                'message' => 'Unauthorized, check your login credentials'
             ], 401);
         }
 
         $user = $request->user();
         $tokenResult = $user->createToken('Personal Access Token');
-        $token = $tokenResult->plaintextToken;
+        $token = $tokenResult->plainTextToken;
 
         return response()->json([
             'token' => $token,
             'user' => $user
         ]);
-
     }
 
-    public function register(Request $request){
+    public function register(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|between:2,100',
             'email' => 'required|string|email|max:100|unique:users',
             'password' => 'required|string|confirmed|min:6'
         ]);
 
-        if($validator->fails()){
+        if ($validator->fails()) {
             return response()->json([
                 'message' => $validator->errors()->first()
             ], 401);
@@ -57,12 +59,19 @@ class ApiController extends Controller
         ));
 
         $tokenResult = $user->createToken('Personal Access Token');
-        $token = $tokenResult->plaintextToken;
+        $token = $tokenResult->plainTextToken;
 
         return response()->json([
-            'message' => "User successfully registered!",
+            'message' => 'User successfully registered!',
             'token' => $token,
-            'user' => $user,
-        ])
+            'user' => $user
+        ]);
+    }
+
+    public function logout(Request $request) {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(
+            ['message' => 'User successfully logged out!']
+        );
     }
 }
