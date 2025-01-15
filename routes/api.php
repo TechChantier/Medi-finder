61522d60590e84864ee3b353208eef7d4aed188c
Suzy Kenne <suzyndatewo@gmail.com>
2025-01-15 17:00:15 +0100

    added routes for each controllers

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController; 
+use App\Http\Controllers\MedicalFacilitiesController; 
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -17,3 +18,5 @@ Route::group(['middleware' => 'auth:sanctum'], function(){
     Route::post('logout', [ApiController::class, 'logout']);
 
 });
+Route::post('medical-facilities', [MedicalFacilitiesController::class, 'store']);
+Route::get('medical-facilities/{id}', [MedicalFacilitiesController::class, 'show']);
