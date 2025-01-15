21ad0999193ca3ceb184881feb3bd02e27fbc17e
Suzy Kenne <suzyndatewo@gmail.com>
2025-01-15 17:00:34 +0100

    updated medical facilities models

---
+++ app/Models/MedicalFacility.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+
+class MedicalFacility extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',       // Add this line
+        'name',
+        'address',
+        'whatsapp_number',
+        'email',
+        'description',
+        'operating_hours',
+        'services',
+        'latitude',
+        'longitude',
+        'status',
+        'units'
+    ];
+
+    
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function medical_facility_units()
+    {
+        return $this->belongsToMany(Unit::class);
+    }
+}
+
+
+
+
