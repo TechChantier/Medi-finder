7e023723411d348135a2eb5fcf3cf9cd24c15c7a
Suzy Kenne <suzyndatewo@gmail.com>
2025-01-24 21:12:13 +0100

    updated all api routes

--- routes/api.php
+++ routes/api.php
@@ -1,47 +1,20 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ApiController; 
-use App\Http\Controllers\MedicalFacilitiesController; 
-use App\Http\Controllers\MedicalFacilitiesUnitController; 
-use App\Http\Controllers\SearchController;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::post('auth/login', [ApiController::class, 'login']);
-Route::post('auth/register', [ApiController::class, 'register']);
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MedicalFacilityController;
+use App\Http\Controllers\UnitController; // Auth Routes
 
 
-Route::group(['middleware' => 'auth:sanctum'], function(){
-
-    Route::post('auth/logout', [ApiController::class, 'logout']);
-
-    Route::get('auth/user', [ApiController::class, 'user']);
-
-    Route::post('medical-facilities', [MedicalFacilitiesController::class, 'store']);
-
-    Route::post('medical-facilities/{facility_id}/units', [MedicalFacilitiesUnitController::class, 'store']);
-
-    Route::put('medical-facilities/{id}', [MedicalFacilitiesController::class, 'update']);
-
-    Route::put('medical-facilities/{facility_id}/units/{id}', [MedicalFacilitiesUnitController::class, 'update']);
-
-    Route::delete('medical-facilities/{id}', [MedicalFacilitiesController::class, 'destroy']);
-
-    Route::delete('medical-facilities/{facility_id}/units/{id}', [MedicalFacilitiesUnitController::class, 'destroy']);
-
-
-
+Route::post('auth/login', [AuthController::class, 'login']);
+Route::post('auth/register', [AuthController::class, 'register']); // Public Routes
+Route::get('medical-facilities', [MedicalFacilityController::class, 'index']);
+Route::get('medical-facilities/{facility}', [MedicalFacilityController::class, 'show']); // Protected Routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('auth/logout', [AuthController::class, 'logout']);
+    Route::get('auth/profile', [AuthController::class, 'profile']);    // Medical Facilities
+    Route::patch('medical-facilities/{facility}', [MedicalFacilityController::class, 'update']);
+    Route::delete('medical-facilities/{facility}', [MedicalFacilityController::class, 'destroy']);
+    Route::get('medical-facilities/search', [MedicalFacilityController::class, 'search']);    // Units
+    Route::apiResource('units', UnitController::class);
 });
-
-Route::get('medical-facilities/search', [MedicalFacilitiesController::class, 'search']);
-
-Route::get('medical-facilities/filter', [MedicalFacilitiesController::class, 'filter']);
-
-Route::get('medical-facilities/{id}', [MedicalFacilitiesController::class, 'show']);
-
-Route::get('medical-facilities/{facility_id}/units/{id}', [MedicalFacilitiesUnitController::class, 'show']);
-
