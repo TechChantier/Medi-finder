83e83f3fb2aca4a0905d0130880a5bc1af2d0326
Suzy Kenne <suzyndatewo@gmail.com>
2025-01-24 21:13:07 +0100

    added ressources

---
+++ app/Http/Resources/UnitResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UnitResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'type' => 'units',
+            'id' => (string)$this->id,
+            'attributes' => [
+                'name' => $this->name,
+                'description' => $this->description,
+                'status' => $this->status,
+                'created_at' => $this->created_at,
+                'updated_at' => $this->updated_at
+            ]
+        ];
+    }
+}
