549146802da66e6d37d758e97d6a8cc307308846
Suzy Kenne <suzyndatewo@gmail.com>
2025-03-07 10:18:21 +0100

    updated base url

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://rrn24.techchantier.site/Medi-finder/public/'),
 
     /*
     |--------------------------------------------------------------------------
